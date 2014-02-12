276254f1f7f1696c320fb52494c646fcbee89a55
Damon Oehlman <damon.oehlman@gmail.com>
2014-02-12 17:53:59 +1100

    Simple connection test

--- test/connect.js
+++ test/connect.js
@@ -5,6 +5,16 @@ var baseUrl = process.env.JANUS_URL || 'http://localhost:8088/janus';
 
 test('create a new session', function(t) {
   t.plan(1);
-  session = new JanusSession(baseUrl);
+  session = new JanusSession();
   t.ok(session instanceof JanusSession, 'session instance created');
+});
+
+test('attempt connection with server', function(t) {
+  t.plan(2);
+
+  // attempt connection
+  session.connect(baseUrl, function(err) {
+    t.ifError(err);
+    t.ok(session.id, 'assigned session id');
+  });
 });
